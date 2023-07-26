0e83d74501bcbe3046a0a2cccf0c85f457723820
Florin Olteanu <olteanuflorin86@gmail.com>
2023-07-26 19:58:21 +0300

    adding Eureka LB endpoints

---
+++ src/main/java/com/olteanuforin86/msscbrewerygatewayv1/config/LoadBalancedRouteConfig.java
@@ -0,0 +1,29 @@
+package com.olteanuforin86.msscbrewerygatewayv1.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@Configuration
+public class LoadBalancedRouteConfig {
+
+	@Bean
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("beer-service", r -> r
+                		.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                        .uri("lb://beer-service"))
+//                        .id("beer-service"))
+                .route("order-service", r -> r
+                		.path("/api/v1/customers/**")
+                        .uri("lb://order-service"))
+                .route("inventory-service", r -> r
+                		.path("/api/v1/beer/*/inventory")
+                        .uri("lb://inventory-service"))
+                .build();
+    }
+	
+}
