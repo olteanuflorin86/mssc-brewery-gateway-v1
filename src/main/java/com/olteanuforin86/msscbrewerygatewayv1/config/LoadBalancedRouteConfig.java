02af662569c96acf73e3313d7736bb7d6a0c16ce
Florin Olteanu <olteanuflorin86@gmail.com>
2023-07-29 18:14:17 +0300

    config res4j for gateway circuit breaker

--- src/main/java/com/olteanuforin86/msscbrewerygatewayv1/config/LoadBalancedRouteConfig.java
+++ src/main/java/com/olteanuforin86/msscbrewerygatewayv1/config/LoadBalancedRouteConfig.java
@@ -22,7 +22,14 @@ public class LoadBalancedRouteConfig {
                         .uri("lb://order-service"))
                 .route("inventory-service", r -> r
                 		.path("/api/v1/beer/*/inventory")
+                		.filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
+                                .setFallbackUri("forward:/inventory-failover")
+                                .setRouteId("inv-failover")
+                            ))
                         .uri("lb://inventory-service"))
+                .route("inventory-failover-service", r -> r
+                		.path("/inventory-failover/**")
+                        .uri("lb://inventory-failover"))
                 .build();
     }
 	
